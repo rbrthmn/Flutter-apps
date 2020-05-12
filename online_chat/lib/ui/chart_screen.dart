51d2d05fa5d9b8c1c0e1f0da078ccb3abe5f496d
Roberto <robertokenzohamano@gmail.com>
2020-05-12 16:53:25 -0300

    Starting seventh project

---
+++ online_chat/lib/ui/chart_screen.dart
@@ -0,0 +1,20 @@
+import 'package:chat/ui/text_composer.dart';
+import 'package:flutter/material.dart';
+
+class ChartScreen extends StatefulWidget {
+  @override
+  _ChartScreenState createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Olá'),
+        elevation: 0,
+      ),
+      body: TextComposer(),
+    );
+  }
+}
