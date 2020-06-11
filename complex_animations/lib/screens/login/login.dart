ec47069044c0c9fb29bdc4008377047a325ca4d5
Roberto <robertokenzohamano@gmail.com>
2020-06-10 21:16:56 -0300

    Creating sign in button

--- complex_animations/lib/screens/login/login.dart
+++ complex_animations/lib/screens/login/login.dart
@@ -2,39 +2,60 @@ import 'package:complexanimations/screens/login/widgets/form_container.dart';
 import 'package:complexanimations/screens/login/widgets/sign_up_button.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/stagger_animation.dart';
+
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/background.jpg"),
-            fit: BoxFit.cover
-          )
-        ),
+            image: DecorationImage(
+                image: AssetImage("images/background.jpg"), fit: BoxFit.cover)),
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             Stack(
+              alignment: Alignment.bottomCenter,
               children: <Widget>[
                 Column(
                   children: <Widget>[
                     Padding(
-                      padding: EdgeInsets.only(top: 70, bottom: 32),
-                      child: Image.asset("images/tickicon.png",
-                      width: 150,
-                      height: 150,
-                      fit: BoxFit.contain,)
-                    ),
+                        padding: EdgeInsets.only(top: 70, bottom: 32),
+                        child: Image.asset(
+                          "images/tickicon.png",
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.contain,
+                        )),
                     FormContainer(),
                     SignUpButton()
                   ],
+                ),
+                StaggerAnimation(
+                  controller: _animationController.view
                 )
               ],
             )
