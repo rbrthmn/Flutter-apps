21ebb9939a2b84b09604f183015f6cf74e9c56dd
Roberto <robertokenzohamano@gmail.com>
2020-06-23 16:54:09 -0300

    Starting project 15

---
+++ todo_mobx/lib/stores/login_store.dart
@@ -0,0 +1,27 @@
+import 'package:mobx/mobx.dart';
+
+part 'login_store.g.dart';
+
+class LoginStore = _LoginStore with _$LoginStore;
+
+abstract class _LoginStore with Store {
+
+  _LoginStore() {
+    autorun((_){
+      print(email);
+    });
+  }
+
+  @observable
+  String email = '';
+
+  @action
+  void setEmail(String value) => email = value;
+
+  @observable
+  String password = '';
+
+  @action
+  void setPassword(String value) => password = value;
+
+}
